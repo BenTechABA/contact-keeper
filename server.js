f20a8f6b64bb03d5ca7423ee673154abe4137d24
BenTechABA <bbosssax2009@gmail.com>
2023-02-18 02:22:27 +0100

    dsds

--- server.js
+++ server.js
@@ -1,26 +1,30 @@
 const express = require('express');
-const connectDB = require('./config/db');
-
+const path = require('path')
+//const { default: mongoose } = require('mongoose');
+//const connectDB = require('./config/db');
 
 const app = express();
-//connect DB
-connectDB();
 
+//connectDB();
+
+// app.post("/", (req, res) => {
+//     res.send('This is it')
+// })
 //init middleware
 app.use(express.json({extended: false}));
-
-
-
-app.get('/', (req, res) => res.json({msg: 'Welcome to contact keeper'}));
-
-
-
-
 // Define Routes
+
 app.use('/api/users', require('./routes/users'));
-app.use('/api/contacts', require('./routes/contacts'));
 app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
+//server static assets in production
+if(process.env.NODE_ENV === 'production'){
+    //Set Static folder
+    app.use(express.static('main/build'));
 
-const PORT = process.env.PORT || 8000;
+    app.get('*', (req, res)=> res.sendFile(path.resolve(__dirname, 'main', 'build', 'index.html')))
+}
+// app.get('/', (req, res) => res.json({msg: 'Welcome to contact keeper'}));
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`server has started on ${PORT}`));
