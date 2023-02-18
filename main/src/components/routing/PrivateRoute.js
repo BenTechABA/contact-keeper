15faa50d12232a9e83db6cccd3766e03577d4868
BenTechABA <bbosssax2009@gmail.com>
2023-02-18 02:35:20 +0100

    dsdsd

--- main/src/components/routing/PrivateRoute.js
+++ main/src/components/routing/PrivateRoute.js
@@ -1,14 +1,14 @@
 import React, { useContext} from 'react'
-import  {Route, Redirect}  from 'react-router-dom'
+import  {Route, redirect}  from 'react-router-dom'
 import AuthContext from '../../context/auth/authContext'
-const PrivateRoute = ({component: Component, ...rest}) => {
+const PrivateRoute = ({element: Element, ...rest}) => {
     const authContext = useContext(AuthContext);
     const {isAuthenticated, loading} = authContext;
   return (
     <Route {...rest} render={props => !isAuthenticated & !loading ? (
-        <Redirect to='/login'/>
+        redirect("/login")
     ):(
-        <Component {...props} />
+        <Element {...props} />
     )}/>
   )
 }
